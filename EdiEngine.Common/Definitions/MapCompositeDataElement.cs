5b45c0b2c665596107257464e6c16b01e5ce1ac7
Jeff Barnes <jbarnes1@users.noreply.github.com>
2020-03-13 09:13:18 -0400

    Add files via upload

---
+++ EdiEngine.Common/Definitions/MapCompositeDataElement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using EdiEngine.Common.Enums;
+
+namespace EdiEngine.Common.Definitions
+{
+    public class MapCompositeDataElement : MapBaseDataElement
+    {
+        public MapCompositeDataElement()
+        {
+            Name = GetType().Name;
+            ReqDes = RequirementDesignator.Optional;
+            Content = new List<MapSimpleDataElement>();
+        }
+
+        public string Name { get; }
+
+        public List<MapSimpleDataElement> Content { get; }
+    }
+}
---
+++ EdiEngine.Common/Definitions/MapSegment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EdiEngine.Common.Definitions
+{
+    public abstract class MapSegment : MapBaseEntity
+    {
+        protected MapSegment()
+        {
+            MaxOccurs = 1;
+            Content = new List<MapBaseDataElement>();
+            SyntaxNotes = new List<string>();
+        }
+
+        public override string EdiName => Name;
+
+        public List<MapBaseDataElement> Content { get; }
+
+        public List<string> SyntaxNotes { get; }
+
+    }
+}
